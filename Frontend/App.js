bf0368b31203415a4969443d7409518abbbb1f15
Jennifer Ye <yej52@mcmaster.ca>
2023-03-30 11:41:11 -0400

    Finish set-up

--- Frontend/App.js
+++ Frontend/App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ff0000',
     alignItems: 'center',
     justifyContent: 'center',
   },
