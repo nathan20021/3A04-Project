a2f214a620a402c45dfda0dc0f49384dedb96f57
Jennifer Ye <yej52@mcmaster.ca>
2023-04-07 02:23:19 -0400

    Adds TaxiSession fair display page

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,17 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import LoginPage from "./Pages/Auth/Login";
-import LoginErrorPage from "./Pages/Auth/LoginError";
-import LoginSuccessPage from "./Pages/Auth/LoginSuccess";
+import LoginPage from "./Pages/Auth/login";
+import LoginErrorPage from "./Pages/Auth/loginError";
+import LoginSuccessPage from "./Pages/Auth/loginSuccess";
+import FareDisplay from "./Pages/TaxiSession/fareDisplay";
+
 
 export default function App() {
   return (
     <View className="flex items-center justify-center flex-1">
       <StatusBar style="auto" />
-      <View className="w-[80%]">
+      <View>
         {/* <LoginPage /> */}
-        <LoginSuccessPage />
-        <LoginErrorPage/>
+        {/* <LoginSuccessPage /> */}
+        <FareDisplay/>
+
       </View>
     </View>
   );
