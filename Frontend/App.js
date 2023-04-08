3104c139c9ca54a0cf1381390a233e9379e68e94
Jennifer Ye <yej52@mcmaster.ca>
2023-04-07 22:23:03 -0400

    Adds page navigation

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,21 +1,7 @@
-import {
-  LoginPage,
-  LoginSuccessPage,
-  LoginErrorPage,
-  LogoutPage,
-} from "./Pages/Auth";
-import {
-  RegistrationPage,
-  RegistrationErrorPage,
-  RegistrationSuccessPage,
-} from "./Pages/Registration";
-import{
-  PromptActivation,
-  PromptDisplay,
-  PromptError
-} from "./Pages/Prompts"
+import {  LoginPage,  LoginSuccessPage,  LoginErrorPage,  LogoutPage } from "./Pages/Auth";
+import {   RegistrationPage,  RegistrationErrorPage,  RegistrationSuccessPage, } from "./Pages/Registration";
+import{ PromptActivation,  PromptDisplay,   PromptError } from "./Pages/Prompts"
 import { LocationSelectionPage } from "./Pages/Dispatcher";
-
 import { PageTransitionConfig as config } from "./config";
 
 import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
@@ -34,63 +20,12 @@ const navTheme = {
 export default function App() {
   return (
     <NavigationContainer theme={navTheme}>
-      <Stack.Navigator initialRouteName="Location Selection">
-        <Stack.Screen name="Registration" component={PromptDisplay} />
-        {/* <Stack.Screen
-          name="Registration Error"
-          component={RegistrationErrorPage}
-          options={{
-            transitionSpec: {
-              open: config,
-              close: config,
-            },
-          }}
-        />
-        <Stack.Screen
-          name="Location Selection"
-          component={LocationSelectionPage}
-          options={{
-            transitionSpec: {
-              open: config,
-              close: config,
-            },
-          }}
-        />
-        <Stack.Screen
-          name="Registration Success"
-          component={RegistrationSuccessPage}
-          options={{
-            transitionSpec: {
-              open: config,
-              close: config,
-            },
-          }}
-        /> */}
+      <Stack.Navigator initialRouteName="Prompt Generator">
+        <Stack.Screen name="Prompt Activate" component={PromptActivation}/>
+        <Stack.Screen name="Prompt Display" component={PromptDisplay} />
+        <Stack.Screen name="Prompt Error" component={PromptError} />
       </Stack.Navigator>
-
-      {/* <Stack.Navigator initialRouteName="Prompt Generator">
-        <Stack.Screen name="Prompt Start" component={PromptActivation} />
-        /* <Stack.Screen
-          name="Prompt Error"
-          component={PromptError}
-          options={{
-            transitionSpec: {
-              open: config,
-              close: config,
-            },
-          }}
-        />
-        <Stack.Screen
-          name="Prompt Display"
-          component={PromptDisplay}
-          options={{
-            transitionSpec: {
-              open: config,
-              close: config,
-            },
-          }}
-        />
-      </Stack.Navigator> */}
     </NavigationContainer>
+
   );
 }
--- Frontend/Pages/Prompts/promptActivation.js
+++ Frontend/Pages/Prompts/promptActivation.js
@@ -1,12 +1,12 @@
-import {Text, View, Pressable, Alert} from 'react-native';
+import {Text, View, Pressable} from 'react-native';
 
-export default PromptActivation = ()=>{
+export default PromptActivation = ({navigation})=>{
     return (
         <View className = "flex-1 items-center justify-center">
           <Text className = "text-2xl text-primary font-medium" >Get talkin!</Text>
           <Text className = "text-4xl mb-16" >Prompt Generator</Text>
           <Pressable className = "h-16 w-32 rounded-xl items-center justify-center bg-primary" 
-                      onPress={() => Alert.alert()}> 
+                      onPress={() =>  navigation.navigate('Prompt Display')}> 
             <Text className = "text-white text-3xl">Start</Text>
           </Pressable>
         </View>
